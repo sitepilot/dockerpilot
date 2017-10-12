df2985d63bb6e67267879abe21ac735f88904eb9
Nick Jansen <n.jansen@sitepilot.nl>
2017-10-12 10:30:43 +0200

    Updated varnish configuration and fix caching issues

--- stacks/lamp/1.0/config/varnish.blade.php
+++ stacks/lamp/1.0/config/varnish.blade.php
@@ -1,324 +1,244 @@
-
-###############################################################################################
-### The perfect Varnish 4.x+ configuration for Joomla, WordPress & other CMS based websites ###
-###############################################################################################
-
-# Varnish Reference:
-# See the VCL chapters in the Users Guide at https://www.varnish-cache.org/docs/
-# and https://www.varnish-cache.org/trac/wiki/VCLExamples for more examples.
 #
+# This is an example VCL file for Varnish.
+#
+# It does not do anything by default, delegating control to the
+# builtin VCL. The builtin VCL is called when there is no explicit
+# return statement.
+#
+# See the VCL chapters in the Users Guide at https://www.varnish-cache.org/docs/
+# and http://varnish-cache.org/trac/wiki/VCLExamples for more examples.
+
+# Update for work with Varnish 4
+
+
 # Marker to tell the VCL compiler that this VCL has been adapted to the
 # new 4.0 format.
 vcl 4.0;
 
-# Imports
-import std;
-
 # Default backend definition. Set this to point to your content server.
 backend default {
-    .host = "sp-app-{{$env['APP_NAME']}}"; # don't change this if the web server is on the same machine
-    .port = "80"; # replace XXXX with your web server's (internal) port, e.g. 8080
+    .host = "sp-app-{{$env['APP_NAME']}}";
+    .port = "80";
+    .connect_timeout = 600s;
+    .first_byte_timeout = 600s;
+    .between_bytes_timeout = 600s;
+    .max_connections = 800;
 }
 
+# Only allow purging from specific IPs
+acl purge {
+    "sp-app-{{$env['APP_NAME']}}";
+    "127.0.0.1";
+	"localhost";
+}
+
+# This function is used when a request is send by a HTTP client (Browser) 
 sub vcl_recv {
+	# Normalize the header, remove the port (in case you're testing this on various TCP ports)
+	set req.http.Host = regsub(req.http.Host, ":[0-9]+", "");
 
-/*
-    # If we host multiple domains on a server, here you can list the domains you DO NOT want to cache
-    # The first check matches both naked & "www" subdomains. Use the second for non generic subdomains.
-    if (
-        req.http.host ~ "(www\.)?(domain1.com|domain2.org|domain3.net)" ||
-        req.http.host ~ "(subdomain.domain4.tld|othersubdomain.domain5.tld)"
-    ) {
-        return (pass);
-    }
-*/
+	# Allow purging from ACL
+	if (req.method == "PURGE") {
+		# If not allowed then a error 405 is returned
+		if (!client.ip ~ purge) {
+			return(synth(405, "This IP is not allowed to send PURGE requests."));
+		}	
+		# If allowed, do a cache_lookup -> vlc_hit() or vlc_miss()
+		return (purge);
+	}
 
-    # Forward client's IP to the backend
-    if (req.restarts == 0) {
-        if (req.http.X-Real-IP) {
-            set req.http.X-Forwarded-For = req.http.X-Real-IP;
-        } else if (req.http.X-Forwarded-For) {
-            set req.http.X-Forwarded-For = req.http.X-Forwarded-For + ", " + client.ip;
-        } else {
-            set req.http.X-Forwarded-For = client.ip;
+	# Post requests will not be cached
+	if (req.http.Authorization || req.method == "POST") {
+		return (pass);
+	}
+
+	# --- Wordpress specific configuration
+	
+	# Did not cache the RSS feed
+	if (req.url ~ "/feed") {
+		return (pass);
+	}
+
+	# Blitz hack
+        if (req.url ~ "/mu-.*") {
+                return (pass);
         }
-    }
 
-    # httpoxy
-    unset req.http.proxy;
+	
+	# Did not cache the admin and login pages
+	if (req.url ~ "/wp-(login|admin)") {
+		return (pass);
+	}
+	
+	 # Do not cache the WooCommerce pages
+	 ### REMOVE IT IF YOU DO NOT USE WOOCOMMERCE ###
+	if (req.url ~ "/(cart|my-account|checkout|addons|/?add-to-cart=)") {
+        	return (pass);
+    	}
 
-    # Normalize the query arguments
-    set req.url = std.querysort(req.url);
+	# Remove the "has_js" cookie
+	set req.http.Cookie = regsuball(req.http.Cookie, "has_js=[^;]+(; )?", "");
 
-    # Non-RFC2616 or CONNECT which is weird.
-    if (
-        req.method != "GET" &&
-        req.method != "HEAD" &&
-        req.method != "PUT" &&
-        req.method != "POST" &&
-        req.method != "TRACE" &&
-        req.method != "OPTIONS" &&
-        req.method != "DELETE"
-    ) {
-        return (pipe);
-    }
+	# Remove any Google Analytics based cookies
+	set req.http.Cookie = regsuball(req.http.Cookie, "__utm.=[^;]+(; )?", "");
 
-    # We only deal with GET and HEAD by default
-    if (req.method != "GET" && req.method != "HEAD") {
-        return (pass);
-    }
+	# Remove the Quant Capital cookies (added by some plugin, all __qca)
+	set req.http.Cookie = regsuball(req.http.Cookie, "__qc.=[^;]+(; )?", "");
 
-    # Remove the wp test cookie
-    if(
-        req.url !~ "^/wp-admin" ||
-        req.url !~ "^/wp-login.php" ||
-        req.url !~ "^/admin"
-    ) {
-      set req.http.Cookie = regsuball(req.http.Cookie, "wordpress_test_cookie=[^;]+(; )?", "");
-    }
+	# Remove the wp-settings-1 cookie
+	set req.http.Cookie = regsuball(req.http.Cookie, "wp-settings-1=[^;]+(; )?", "");
 
-    # Don't cache HTTP authorization/authentication pages and pages with certain headers or cookies
-    if (
-        req.http.Authorization ||
-        req.http.Authenticate ||
-        req.http.X-Logged-In == "True" ||
-        req.http.Cookie ~ "userID" ||
-        req.http.Cookie ~ "joomla_[a-zA-Z0-9_]+" ||
-        req.http.Cookie ~ "(wordpress_[a-zA-Z0-9_]+|wp-postpass|comment_author_[a-zA-Z0-9_]+)"
-    ) {
-        #set req.http.Cache-Control = "private, max-age=0, no-cache, no-store";
-        #set req.http.Expires = "Mon, 01 Jan 2001 00:00:00 GMT";
-        #set req.http.Pragma = "no-cache";
-        return (pass);
-    }
+	# Remove the wp-settings-time-1 cookie
+	set req.http.Cookie = regsuball(req.http.Cookie, "wp-settings-time-1=[^;]+(; )?", "");
 
-    # Exclude the following paths (e.g. backend admins, user pages or ad URLs that require tracking)
-    # In Joomla specifically, you are advised to create specific entry points (URLs) for users to
-    # interact with the site (either common user logins or even commenting), e.g. make a menu item
-    # to point to a user login page (e.g. /login), including all related functionality such as
-    # password reset, email reminder and so on.
-    if(
-        req.url ~ "^/administrator" ||
-        req.url ~ "^/component/banners" ||
-        req.url ~ "^/component/socialconnect" ||
-        req.url ~ "^/component/users" ||
-        req.url ~ "^/contact" ||
-        req.url ~ "^/connect" ||
-        req.url ~ "^/wp-admin" ||
-        req.url ~ "^/wp-login.php" ||
-        req.url ~ "^/admin"
-    ) {
-        #set req.http.Cache-Control = "private, max-age=0, no-cache, no-store";
-        #set req.http.Expires = "Mon, 01 Jan 2001 00:00:00 GMT";
-        #set req.http.Pragma = "no-cache";
-        return (pass);
-    }
+	# Remove the wp test cookie
+	set req.http.Cookie = regsuball(req.http.Cookie, "wordpress_test_cookie=[^;]+(; )?", "");
 
-    # Don't cache ajax requests
-    if(req.http.X-Requested-With == "XMLHttpRequest" || req.url ~ "nocache") {
-        #set req.http.Cache-Control = "private, max-age=0, no-cache, no-store";
-        #set req.http.Expires = "Mon, 01 Jan 2001 00:00:00 GMT";
-        #set req.http.Pragma = "no-cache";
-        return (pass);
-    }
+	# Are there cookies left with only spaces or that are empty?
+	if (req.http.cookie ~ "^ *$") {
+		    unset req.http.cookie;
+	}
+	
+	# Cache the following files extensions 
+	if (req.url ~ "\.(css|js|png|gif|jp(e)?g|swf|ico)") {
+		unset req.http.cookie;
+	}
 
-    # Check for the custom "X-Logged-In" header (used by K2 and other apps) to identify
-    # if the visitor is a guest, then unset any cookie (including session cookies) provided
-    # it's not a POST request.
-    if(req.http.X-Logged-In == "False" && req.method != "POST") {
-        unset req.http.Cookie;
-    }
+	# Normalize Accept-Encoding header and compression
+	# https://www.varnish-cache.org/docs/3.0/tutorial/vary.html
+	if (req.http.Accept-Encoding) {
+		# Do no compress compressed files...
+		if (req.url ~ "\.(jpg|png|gif|gz|tgz|bz2|tbz|mp3|ogg)$") {
+			   	unset req.http.Accept-Encoding;
+		} elsif (req.http.Accept-Encoding ~ "gzip") {
+		    	set req.http.Accept-Encoding = "gzip";
+		} elsif (req.http.Accept-Encoding ~ "deflate") {
+		    	set req.http.Accept-Encoding = "deflate";
+		} else {
+			unset req.http.Accept-Encoding;
+		}
+	}
 
-    # Properly handle different encoding types
-    if (req.http.Accept-Encoding) {
-      if (req.url ~ "\.(jpg|jpeg|png|gif|gz|tgz|bz2|tbz|mp3|ogg|swf)$") {
-        # No point in compressing these
-        unset req.http.Accept-Encoding;
-      } elseif (req.http.Accept-Encoding ~ "gzip") {
-        set req.http.Accept-Encoding = "gzip";
-      } elseif (req.http.Accept-Encoding ~ "deflate") {
-        set req.http.Accept-Encoding = "deflate";
-      } else {
-        # unknown algorithm (aka crappy browser)
-        unset req.http.Accept-Encoding;
-      }
-    }
+	# Check the cookies for wordpress-specific items
+	if (req.http.Cookie ~ "wordpress_" || req.http.Cookie ~ "comment_") {
+		return (pass);
+	}
+	if (!req.http.cookie) {
+		unset req.http.cookie;
+	}
+	
+	# --- End of Wordpress specific configuration
 
-    # Cache files with these extensions
-    #if (req.url ~ "\.(js|css|jpg|jpeg|png|gif|gz|tgz|bz2|tbz|mp3|ogg|swf)$") {
-    #    return (hash);
-    #}
+	# Did not cache HTTP authentication and HTTP Cookie
+	if (req.http.Authorization || req.http.Cookie) {
+		# Not cacheable by default
+		return (pass);
+	}
 
-    # Remove all cookies for static files & deliver directly
-    if (req.url ~ "^[^?]*\.(7z|avi|bmp|bz2|css|csv|doc|docx|eot|flac|flv|gif|gz|ico|jpeg|jpg|js|less|mka|mkv|mov|mp3|mp4|mpeg|mpg|odt|ogg|ogm|opus|otf|pdf|png|ppt|pptx|rar|rtf|svg|svgz|swf|tar|tbz|tgz|ttf|txt|txz|wav|webm|webp|woff|woff2|xls|xlsx|xml|xz|zip)(\?.*)?$") {
-        unset req.http.Cookie;
-        return (hash);
-    }
-
-    return (hash);
-
+	# Cache all others requests
+	return (hash);
 }
+ 
+sub vcl_pipe {
+	return (pipe);
+}
+ 
+sub vcl_pass {
+	return (fetch);
+}
+ 
+# The data on which the hashing will take place
+sub vcl_hash {
+ 	hash_data(req.url);
+ 	if (req.http.host) {
+     	hash_data(req.http.host);
+ 	} else {
+     	hash_data(server.ip);
+ 	}
 
+	# If the client supports compression, keep that in a different cache
+    	if (req.http.Accept-Encoding) {
+        	hash_data(req.http.Accept-Encoding);
+	}
+     
+	return (lookup);
+}
+ 
+# This function is used when a request is sent by our backend (Nginx server)
 sub vcl_backend_response {
+	# Remove some headers we never want to see
+	unset beresp.http.Server;
+	unset beresp.http.X-Powered-By;
 
-/*
-    # If we host multiple domains on a server, here you can list the domains you DO NOT want to cache
-    # The first check matches both naked & "www" subdomains. Use the second for non generic subdomains.
-    if (
-        bereq.http.host ~ "(www\.)?(domain1.com|domain2.org|domain3.net)" ||
-        bereq.http.host ~ "(subdomain.domain4.tld|othersubdomain.domain5.tld)"
-    ) {
-        set beresp.uncacheable = true;
-        return (deliver);
-    }
-*/
+	# For static content strip all backend cookies
+	if (bereq.url ~ "\.(css|js|png|gif|jp(e?)g)|swf|ico") {
+		unset beresp.http.cookie;
+	}
 
-    # Don't cache 50x responses
-    if (
-        beresp.status == 500 ||
-        beresp.status == 502 ||
-        beresp.status == 503 ||
-        beresp.status == 504
-    ) {
-        return (abandon);
-    }
+	# Only allow cookies to be set if we're in admin area
+	if (beresp.http.Set-Cookie && bereq.url !~ "^/wp-(login|admin)") {
+        	unset beresp.http.Set-Cookie;
+    	}
 
-    # Exclude the following paths (e.g. backend admins, user pages or ad URLs that require tracking)
-    # In Joomla specifically, you are advised to create specific entry points (URLs) for users to
-    # interact with the site (either common user logins or even commenting), e.g. make a menu item
-    # to point to a user login page (e.g. /login), including all related functionality such as
-    # password reset, email reminder and so on.
-    if(
-        bereq.url ~ "^/administrator" ||
-        bereq.url ~ "^/component/banners" ||
-        bereq.url ~ "^/component/socialconnect" ||
-        bereq.url ~ "^/component/users" ||
-        bereq.url ~ "^/contact" ||
-        bereq.url ~ "^/connect" ||
-        bereq.url ~ "^/wp-admin" ||
-        bereq.url ~ "^/wp-login.php"
-    ) {
-        #set beresp.http.Cache-Control = "private, max-age=0, no-cache, no-store";
-        #set beresp.http.Expires = "Mon, 01 Jan 2001 00:00:00 GMT";
-        #set beresp.http.Pragma = "no-cache";
-        set beresp.uncacheable = true;
-        return (deliver);
-    }
+	# don't cache response to posted requests or those with basic auth
+	if ( bereq.method == "POST" || bereq.http.Authorization ) {
+        	set beresp.uncacheable = true;
+		set beresp.ttl = 120s;
+		return (deliver);
+    	}
+ 
+    	# don't cache search results
+	if ( bereq.url ~ "\?s=" ){
+		set beresp.uncacheable = true;
+                set beresp.ttl = 120s;
+                return (deliver);
+	}
+    
+	# only cache status ok
+	if ( beresp.status != 200 ) {
+		set beresp.uncacheable = true;
+                set beresp.ttl = 120s;
+                return (deliver);
+	}
 
-    # Don't cache HTTP authorization/authentication pages and pages with certain headers or cookies
-    if (
-        bereq.http.Authorization ||
-        bereq.http.Authenticate ||
-        bereq.http.X-Logged-In == "True" ||
-        bereq.http.Cookie ~ "userID" ||
-        bereq.http.Cookie ~ "joomla_[a-zA-Z0-9_]+" ||
-        bereq.http.Cookie ~ "(wordpress_[a-zA-Z0-9_]+|wp-postpass|comment_author_[a-zA-Z0-9_]+)"
-    ) {
-        #set beresp.http.Cache-Control = "private, max-age=0, no-cache, no-store";
-        #set beresp.http.Expires = "Mon, 01 Jan 2001 00:00:00 GMT";
-        #set beresp.http.Pragma = "no-cache";
-        set beresp.uncacheable = true;
-        return (deliver);
-    }
-
-    # Don't cache ajax requests
-    if(beresp.http.X-Requested-With == "XMLHttpRequest" || bereq.url ~ "nocache") {
-        #set beresp.http.Cache-Control = "private, max-age=0, no-cache, no-store";
-        #set beresp.http.Expires = "Mon, 01 Jan 2001 00:00:00 GMT";
-        #set beresp.http.Pragma = "no-cache";
-        set beresp.uncacheable = true;
-        return (deliver);
-    }
-
-    # Don't cache backend response to posted requests
-    if (bereq.method == "POST") {
-        set beresp.uncacheable = true;
-        return (deliver);
-    }
-
-    # Ok, we're cool & ready to cache things
-    # so let's clean up some headers and cookies
-    # to maximize caching.
-
-    # Check for the custom "X-Logged-In" header to identify if the visitor is a guest,
-    # then unset any cookie (including session cookies) provided it's not a POST request.
-    if(bereq.method != "POST" && beresp.http.X-Logged-In == "False") {
-        unset beresp.http.Set-Cookie;
-    }
-
-    # Unset the "etag" header (suggested)
-    unset beresp.http.etag;
-
-    # Unset the "pragma" header
-    unset beresp.http.Pragma;
-
-    # Allow stale content, in case the backend goes down
-    set beresp.grace = 6h;
-
-    # This is how long Varnish will keep cached content
-    set beresp.ttl = {{ (! empty($env['APP_VARNISH_TTL']) ? $env['APP_VARNISH_TTL'] : '2m') }};
-
-    # Modify "expires" header - https://www.varnish-cache.org/trac/wiki/VCLExampleSetExpires
-    #set beresp.http.Expires = "" + (now + beresp.ttl);
-
-    # If your backend server does not set the right caching headers for static assets,
-    # you can set them below (uncomment first and change 604800 - which 1 week - to whatever you
-    # want (in seconds)
-    #if (req.url ~ "\.(ico|jpg|jpeg|gif|png|bmp|webp|tiff|svg|svgz|pdf|mp3|flac|ogg|mid|midi|wav|mp4|webm|mkv|ogv|wmv|eot|otf|woff|ttf|rss|atom|zip|7z|tgz|gz|rar|bz2|tar|exe|doc|docx|xls|xlsx|ppt|pptx|rtf|odt|ods|odp)(\?[a-zA-Z0-9=]+)$") {
-    #    set beresp.http.Cache-Control = "public, max-age=604800";
-    #}
-
-    if (bereq.url ~ "^[^?]*\.(7z|avi|bmp|bz2|css|csv|doc|docx|eot|flac|flv|gif|gz|ico|jpeg|jpg|js|less|mka|mkv|mov|mp3|mp4|mpeg|mpg|odt|ogg|ogm|opus|otf|pdf|png|ppt|pptx|rar|rtf|svg|svgz|swf|tar|tbz|tgz|ttf|txt|txz|wav|webm|webp|woff|woff2|xls|xlsx|xml|xz|zip)(\?.*)?$") {
-        unset beresp.http.set-cookie;
-        set beresp.do_stream = true;
-    }
-
-    # We have content to cache, but it's got no-cache or other Cache-Control values sent
-    # So let's reset it to our main caching time (2m as used in this example configuration)
-    # The additional parameters specified (stale-while-revalidate & stale-if-error) are used
-    # by modern browsers to better control caching. Set there to twice & five times your main
-    # cache time respectively.
-    # This final setting will normalize CMSs like Joomla which set max-age=0 even when
-    # Joomla's cache is enabled.
-    if (beresp.http.Cache-Control !~ "max-age" || beresp.http.Cache-Control ~ "max-age=0") {
-        set beresp.http.Cache-Control = "public, max-age=120, stale-while-revalidate=240, stale-if-error=480";
-    }
-
-    return (deliver);
-
+	# Set TTL
+	set beresp.ttl = {{ (! empty($env['APP_VARNISH_TTL']) ? $env['APP_VARNISH_TTL'] : '2m') }};
+	
+	# Define the default grace period to serve cached content
+	set beresp.grace = 30s;
+	
+	return (deliver);
 }
-
+ 
+# The routine when we deliver the HTTP request to the user
+# Last chance to modify headers that are sent to the client
 sub vcl_deliver {
+	if (obj.hits > 0) { 
+		set resp.http.X-Cache = "cached";
+	} else {
+		set resp.http.x-Cache = "uncached";
+	}
 
-/*
-    # Send a special header for excluded domains only
-    # The if statement can be identical to the ones in the vcl_recv() and vcl_fetch() functions above
-    if (
-        req.http.host ~ "(www\.)?(domain1.com|domain2.org|domain3.net)" ||
-        req.http.host ~ "(subdomain.domain4.tld|othersubdomain.domain5.tld)"
-    ) {
-        set resp.http.X-Domain-Status = "EXCLUDED";
-    }
+	# Remove some headers: PHP version
+	unset resp.http.X-Powered-By;
 
-    # Enforce redirect to HTTPS for specified domains only
-    if (
-        req.http.host ~ "(subdomain.domain4.tld|othersubdomain.domain5.tld)" &&
-        req.http.X-Forwarded-Proto !~ "(?i)https"
-    ) {
-        set resp.http.Location = "https://" + req.http.host + req.url;
-        set resp.status = 302;
-    }
-*/
-    # Send special headers that indicate the cache status of each web page
-    if (obj.hits > 0) {
-        set resp.http.X-Cache = "HIT";
-        set resp.http.X-Cache-Hits = obj.hits;
-    } else {
-        set resp.http.X-Cache = "MISS";
-    }
+	# Remove some headers: Apache version & OS
+	unset resp.http.Server;
 
-    set resp.http.X-Powered-By = "Sitepilot (sitepilot.io)";
+	# Remove some heanders: Varnish
+	unset resp.http.Via;
+	unset resp.http.X-Varnish;
+	
+	# Add powered by header
+	set resp.http.X-Powered-By = "Sitepilot (sitepilot.io)";
 
-    return (deliver);
-
+	return (deliver);
+}
+ 
+sub vcl_init {
+ 	return (ok);
+}
+ 
+sub vcl_fini {
+ 	return (ok);
 }
