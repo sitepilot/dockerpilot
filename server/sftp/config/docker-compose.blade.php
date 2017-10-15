3dc3b119c63ae5323aa0276571343ba2191a722a
Nick Jansen <n.jansen@sitepilot.nl>
2017-10-15 17:26:12 +0200

    Added fail2ban config mount

--- server/sftp/config/docker-compose.blade.php
+++ server/sftp/config/docker-compose.blade.php
@@ -9,6 +9,9 @@ services:
 {{ $sftpAppVolumes }}
         - ./users.conf:/etc/sftp/users.conf:ro
         {{ ! sp_is_windows() ? "- /var/log/syslog:/var/log/serverpilot_syslog" : "" }}
+        {{ ! sp_is_windows() ? "- ".SERVER_WORKDIR."/config/fail2ban/jail.local:/etc/fail2ban/jail.local" : "" }}
+        {{ ! sp_is_windows() ? "- ".SERVER_WORKDIR."/config/fail2ban/fail2ban.local:/etc/fail2ban/fail2ban.local" : "" }}
+        {{ ! sp_is_windows() ? "- /etc/timezone:/etc/timezone.host:ro" : "" }}
     ports:
         - "2222:22"
     {{ ! sp_is_windows() ? "
