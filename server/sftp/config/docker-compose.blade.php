6dc4d91c8b713dfe28717b711c418fc079c131e9
Nick Jansen <n.jansen@sitepilot.nl>
2017-10-15 18:55:57 +0200

    Added fail2ban docker sftp config

--- server/sftp/config/docker-compose.blade.php
+++ server/sftp/config/docker-compose.blade.php
@@ -11,12 +11,13 @@ services:
         {{ ! sp_is_windows() ? "- /var/log/syslog:/var/log/serverpilot_syslog" : "" }}
         {{ ! sp_is_windows() ? "- ".SERVER_WORKDIR."/config/fail2ban/jail.local:/etc/fail2ban/jail.local" : "" }}
         {{ ! sp_is_windows() ? "- ".SERVER_WORKDIR."/config/fail2ban/fail2ban.local:/etc/fail2ban/fail2ban.local" : "" }}
-        {{ ! sp_is_windows() ? "- /etc/timezone:/etc/timezone.host:ro" : "" }}
+        {{ ! sp_is_windows() ? "- ".SERVER_WORKDIR."/config/fail2ban/docker-sftp.conf:/etc/fail2ban/filter.d/docker-sftp.conf" : "" }}
     ports:
         - "2222:22"
     {{ ! sp_is_windows() ? "
     logging:
         driver: syslog" : "" }}
+    {{ ! sp_is_windows() ? "privileged: true" : "" }}
 
 networks:
   default:
