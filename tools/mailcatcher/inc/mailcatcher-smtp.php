6024ebe5a894c3948622a273bf5722746c7a5f66
Nick Jansen <n.jansen@sitepilot.nl>
2017-10-23 20:25:43 +0200

    Fix mailcatcher hostname

--- tools/mailcatcher/inc/mailcatcher-smtp.php
+++ tools/mailcatcher/inc/mailcatcher-smtp.php
@@ -9,6 +9,6 @@ add_action( 'phpmailer_init', 'send_smtp_email' );
 function send_smtp_email( $phpmailer ) {
 	$phpmailer->isSMTP();
 	$phpmailer->SMTPAuth = false;
-	$phpmailer->Host = "serverpilot-mailcatcher";
+	$phpmailer->Host = "sp-mailcatcher";
 	$phpmailer->Port = "1025";
 }
