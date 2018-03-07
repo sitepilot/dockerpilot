2890b924aa65cd4ffad13a2e73a19154054fa059
Nick Jansen <n.jansen@chasemail.nl>
2018-03-07 15:06:20 +0100

    Made app CPU and Memory optional and removed old Varnish config

--- source/stacks/lamp/1.0/config/docker-compose.blade.php
+++ source/stacks/lamp/1.0/config/docker-compose.blade.php
@@ -12,16 +12,16 @@ services:
       - 80
     restart: always
     environment:
-      {{ ! isset($env['APP_VARNISH']) || $env['APP_VARNISH'] == 'off' ? "VIRTUAL_HOST: " . $env['APP_DOMAINS'] : "" }}
-      {{ (! isset($env['APP_VARNISH']) || $env['APP_VARNISH'] == 'off') && ! empty($env['APP_SSL_DOMAINS']) ? "LETSENCRYPT_HOST: " . $env['APP_SSL_DOMAINS'] : "" }}
-      {{ (! isset($env['APP_VARNISH']) || $env['APP_VARNISH'] == 'off') && ! empty($env['APP_SSL_EMAIL']) ? "LETSENCRYPT_EMAIL: ".$env['APP_SSL_EMAIL'] : "" }}
+      {{ "VIRTUAL_HOST: " . $env['APP_DOMAINS'] }}
+      {{ ! empty($env['APP_SSL_DOMAINS']) ? "LETSENCRYPT_HOST: " . $env['APP_SSL_DOMAINS'] : "" }}
+      {{ ! empty($env['APP_SSL_EMAIL']) ? "LETSENCRYPT_EMAIL: " . $env['APP_SSL_EMAIL'] : "" }}
     volumes:
       - ./ssmtp.conf:/etc/ssmtp/ssmtp.conf
       - ./app:{{$env['APP_MOUNT_POINT']}}:cached
       - ./php.ini:/usr/local/etc/php/php.ini
       {{ ! empty($env['APP_VOLUME_1']) ? "- " . $env['APP_VOLUME_1'] : "" }}
-    cpus: {{ ! empty($env['APP_CPUS']) ? $env['APP_CPUS'] : "1" }}
-    mem_limit: {{ ! empty($env['APP_MEMORY']) ? $env['APP_MEMORY'] * 1000000 : 512 * 1000000 }}
+    {{ ! empty($env['APP_CPUS']) ? "cpus: " . $env['APP_CPUS'] : "" }}
+    {{ ! empty($env['APP_MEMORY']) ? "mem_limit: " . ($env['APP_MEMORY'] * 1000000 / 512 * 1000000) : "" }}
 
 networks:
   default:
