2fef4bf5f3fe841e29c81d3ca1c555e115d271a9
Nick Jansen <n.jansen@chasemail.nl>
2018-04-17 15:57:54 +0200

    Fix import

--- commands/BackupRunCommand.php
+++ commands/BackupRunCommand.php
@@ -3,6 +3,7 @@
 namespace Dockerpilot\Command;
 
 use Exception;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
@@ -80,7 +81,7 @@ class BackupRunCommand extends DockerpilotCommand
         $apps = dp_get_apps();
 
         foreach($apps as $app) {
-            $output->writeln('Backup: ' . $app . '...');
+            $output->writeln('Backup ' . $app . '...');
             $command = $this->getApplication()->find('app:backup');
             $arguments = array(
                 'app' => $app
