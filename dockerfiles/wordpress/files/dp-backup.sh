cc60414fc9d838450f68e1205678d755a7ffe970
Nick Jansen <n.jansen@chasemail.nl>
2018-04-18 10:50:36 +0200

    Added application database backup

---
+++ dockerfiles/wordpress/files/dp-backup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+MYSQLDUMP=/usr/bin/mysqldump
+BACKUP_FILE="/var/www/backup/mysql/$APP_NAME.gz"
+
+echo "Backup database ($BACKUP_FILE)..."
+mkdir -p "$BACKUP_DIR/mysql"
+$MYSQLDUMP --force --opt -h$APP_DB_HOST --user=$APP_NAME -p$APP_DB_PASS --databases $APP_NAME | gzip > $BACKUP_FILE
+echo "Done!"
