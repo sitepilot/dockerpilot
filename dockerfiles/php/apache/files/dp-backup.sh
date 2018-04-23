b5a28b5746614d451cee91bd4c1d91463cb502db
Nick Jansen <n.jansen@sitepilot.nl>
2018-04-23 22:49:15 +0200

    Added php-apache Docker config

---
+++ dockerfiles/php/apache/files/dp-backup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+MYSQLDUMP=/usr/bin/mysqldump
+BACKUP_DIR="/var/www/backup/mysql"
+BACKUP_FILE="$BACKUP_DIR/$APP_DB_NAME.gz"
+
+echo "Backup database ($BACKUP_FILE)..."
+mkdir -p $BACKUP_DIR
+$MYSQLDUMP --force --opt -h$APP_DB_HOST --user=$APP_DB_USER -p$APP_DB_PASS --databases $APP_DB_NAME | gzip > $BACKUP_FILE
+echo "Done!"
